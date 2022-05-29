b0717d7f0cc07a7557c1079df7dbc205ad8a2b69
Jereamy Smothers <jereamyasmothers@gmail.com>
2022-05-28 18:45:52 -0700

    fixed the script and just like that it works

--- Develop/script.js
+++ Develop/script.js
@@ -3,57 +3,57 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-var generateABCUp = ['A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z']
-var generateABCL = ['a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z']
-var numbers = ['1, 2, 3, 4, 5, 6, 7, 8, 9, 0']
-var specialC = ['~!@#$%^&*()-_=+[{]}\|;:,<.>/?] ']
+var generateABCUp = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+var generateABCL = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+var specialC = ['~', ',', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '{', ']', '}']
 
 // Write password to the #password input
 function writePassword() {
 
-  var password = generatePassword();
+  var password = '';
   var passwordText = document.querySelector("#password");
-  if (generateBtn) {
-    Math.random(generateABCL)
-    window.prompt('How many characters would you like? (8-128)');
-    var generatePassLength = window.prompt;
-    if(generatePassLength > 8 || < 128){
+  
 
-      window.confirm('Would you like to include Uppercase letters?');
-      if(window.confirm){
-        Math.random(generateABCUp);
+  var generatePassLength = window.prompt('How many characters?(8-128)');
+  if (generatePassLength >= 8 && generatePassLength <= 128) {
 
-        for(var generateABCUp=0; generateABCUp< generateABCUp.length; generateABCUp++){
-          console.log
-        }
+    var checkforUppercase = window.confirm('Would you like to include Uppercase letters?');
+    var checkforLowercase = window.confirm('would you like to add lowercase letter?');
+    var checkforNum = window.confirm('Would you like to include numbers?');
+    var checkSpec = window.confirm('Would you like to include special characters?');
+
+    for (let i = 0; i < generatePassLength; i++) {
+
+      if (checkforUppercase && password.length < generatePassLength) {
+        var index = Math.floor(Math.random() * 26);
+        password = password + generateABCUp[index];
+      };
+
+
+      if (checkforLowercase && password.length < generatePassLength) {
+        var indexLow = Math.floor(Math.random() * 26);
+        password = password + generateABCL[indexLow];
+      };
+
+
+      if (checkforNum && password.length < generatePassLength) {
+        var indexNum = Math.floor(Math.random() * 10);
+        password = password + numbers[indexNum];
+
       }
 
-      window.confirm('Would you like to include numbers?')
-      if(window.confirm){
-        Math.random(numbers);
 
-        for(var numbers=0; numbers< numbers.length; numbers++){
-          console.log
-        }
+      if (checkSpec && password.length < generatePassLength) {
+        var indexSpec = Math.floor(Math.random() * specialC.length);
+        password = password + specialC[indexSpec];
+
       }
-
-      window.confirm('Would you like to include special characters?')
-      if(window.confirm){
-        Math.random(specialC);
-
-        for(var specialC=0; specialC< specialC.length; numbers++){
-          console.log
-        }
-      }
-
-      Math.add(console.log = generatePassLength);
     }
   }
 
   passwordText.value = password;
-  return writePassword;
 }
 
-generate.writePassword
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
